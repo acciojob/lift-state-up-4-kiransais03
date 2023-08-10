b4ae8efd6b45792d5760d1ac8b6dac62f99ea19b
kiransai <kiransais03@gmail.com>
2023-08-10 15:38:54 +0530

    submitting code-Thu Aug 10 2023 15:38:50 GMT+0530 (India Standard Time)

--- src/components/Child.js
+++ src/components/Child.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const Child=({cartitems,setCartitems})=>{
 
-    function removelogic(event) {
-      
+    function removelogic(event,index) {
+      let remitems = cartitems.filter((elem,ind)=>{console.log(elem,ind,index); return (ind!==index)})
+      setCartitems(remitems)
+      console.log("done",cartitems)
     }
 
     return(<div className="child ">
     <h2>Child Component</h2>
     <ul>
-    {cartitems && cartitems.map((elementobj,index)=>{return(<li key={index}>{elementobj.item}-${elementobj.price} <button onClick={(e)=>{removelogic(e)}}>Remove</button></li>)})}
+    {cartitems && cartitems.map((elementobj,index)=>{return(<li key={index}>{elementobj.item}-${elementobj.price} <button onClick={(e)=>{removelogic(e,index)}}>Remove</button></li>)})}
     </ul>
     </div>)
 }
